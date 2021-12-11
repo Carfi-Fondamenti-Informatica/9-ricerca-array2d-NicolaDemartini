35506bc61711ba5cc0d10d539d0a1c855854552b
NicolaDemartini <91873902+NicolaDemartini@users.noreply.github.com>
2021-12-11 21:22:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista[10][20] = {0};
+    char nome[20] = {0};
+    for (size_t i = 0; i < 10; i++) cin >> lista[i];
+    cout<< endl;
+    nome[20];
+    for (int j=0; j<20; j++) nome[j];
+    cin >> nome;
+    int posizione = cerca(lista,nome);
+    if (posizione >= 0) {
+        cout<<  posizione << endl;
+    } else {
+        cout << "non presente" << endl;
+    }
 }
